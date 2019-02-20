01df96fa7ee5bf4189fbfbbad3d9099b6ffc0e2a
robertguitar20 <roborobert16@gmail.com>
2019-02-20 17:54:05 +0400

    ///////{}\\\\\\

---
+++ src/js/index.js
@@ -0,0 +1,93 @@
+import axios from "axios";
+
+const ALBUMS_LINK = "https://jsonplaceholder.typicode.com/albums",
+  ALBUMS_ID_LINK = "https://jsonplaceholder.typicode.com/photos?albumId=",
+  LIST_CONTENT = document.querySelector(".list__content");
+
+let albumList = document.querySelector(".list"),
+  listGrid = document.querySelector(".list__grid");
+
+addEventListener("load", getData);
+
+/// ________________ Davaleba 2 __________________ ///
+/**
+ * Get All albums
+ * @param {Array} data
+ */
+function displayAlbumList(data) {
+  if (data && data.length !== 0) {
+    data.forEach(el => {
+      albumList.innerHTML += `
+        <li class="list__item" data-id="${el.id}">${el.title}</li>
+    `;
+    });
+  }
+}
+
+/// ____________________________________________________________________///
+
+/// ________________ Davaleba 3 __________________ ///
+
+// Get data after load
+function getData() {
+  let hasLoad = false;
+  axios
+    .get(ALBUMS_LINK)
+    .then(res => {
+      if (res.status === 200 && !hasLoad) {
+        hasLoad = true;
+        return displayAlbumList(res.data);
+      } else {
+        LIST_CONTENT.textContent = "მონაცემები იტვრთება";
+      }
+    })
+    .then(_ => {
+      document
+        .querySelectorAll(".list__item")
+        .forEach(el => el.addEventListener("click", getAlbums));
+    });
+}
+
+/**
+ * Get Data by id
+ * @param {Array} data
+ */
+function displayAlbumThumbnail(data) {
+  if (data && data.length !== 0) {
+    data.forEach(el => {
+      listGrid.innerHTML += `
+        <li class="list__grid__item" data-id="${el.id}">
+           <a href="${el.thumbnailUrl}" target="_blank">
+            <img src="${el.thumbnailUrl}" />
+            </a>
+        </li>
+    `;
+    });
+  }
+}
+// Get Albums Thumbnail
+function getAlbums(e) {
+  let target = e.target,
+    id = target.getAttribute("data-id");
+
+  if (id) {
+    axios.get(ALBUMS_ID_LINK + id).then(res => {
+      return displayAlbumThumbnail(res.data);
+    });
+  }
+}
+/// ____________________________________________________________________///
+
+/// ________________ Davaleba 4 __________________ ///
+function* generator(i) {
+  if (i % 2 === 0) {
+    yield i;
+  }
+}
+
+for (let i = 0; i <= 10; i++) {
+  if (generator(i).next().value !== undefined) {
+    console.log(generator(i).next().value);
+  }
+}
+/// ____________________________________________________________________///
